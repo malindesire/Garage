875d36ecafb2023b0a098172f79d9616bc796f7d
malindesire <malindesirejohansson@gmail.com>
2025-06-16 16:17:57 +0200

    Add class Spot

---
+++ ConsoleApp/Spot.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp.Vehicles;
+
+namespace ConsoleApp
+{
+    // Represents a parking spot in the parking lot
+    // Each spot has a unique number and can either be occupied by a vehicle or not
+    internal class Spot
+    {
+        // TODO: Make sure spot number is unique across all spots.
+        public int Number { get; } // Spot number, unique identifier for the spot
+        public Vehicle? ParkedVehicle { get; private set; }
+
+        public bool IsOccupied => ParkedVehicle != null;
+
+        public Spot(int number)
+        {
+            Number = number;
+        }
+
+        public bool Park(Vehicle vehicle)
+        {
+            if (IsOccupied)
+            {
+                return false; // Spot already taken
+            }
+
+            ParkedVehicle = vehicle;
+            return true;
+        }
+
+        public void Vacate()
+        {
+            ParkedVehicle = null;
+        }
+    }
+}
