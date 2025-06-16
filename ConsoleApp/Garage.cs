b26dc82ea2b2a4dd9fbd3fc1d458c168a36d7c68
malindesire <malindesirejohansson@gmail.com>
2025-06-16 17:07:04 +0200

    Add generic class Garage

---
+++ ConsoleApp/Garage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace ConsoleApp
+{
+    internal class Garage<T> : IEnumerable<T>
+    {
+        private readonly T[] _vehicles;
+        public int Capacity { get; }
+
+        public Garage(int capacity)
+        {
+            Capacity = capacity;
+            _vehicles = new T[capacity];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in _vehicles)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
