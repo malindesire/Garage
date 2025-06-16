a8e4b27d39432c1f4aed00a7cfb32c95ea74390f
malindesire <malindesirejohansson@gmail.com>
2025-06-16 15:26:42 +0200

    Add abstract class Vehicle

---
+++ ConsoleApp/Vehicles/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.Vehicles
+{
+    internal abstract class Vehicle
+    {
+        public string RegNumber { get; }
+        public string Color { get; }
+        public int Wheels { get; }
+
+        public Vehicle(string regNumber, string color, int wheels)
+        {
+            RegNumber = regNumber;
+            Color = color;
+            Wheels = wheels;
+        }
+    }
+}
