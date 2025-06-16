7915e32db0660df249feb05d89e6048fa4485c86
malindesire <malindesirejohansson@gmail.com>
2025-06-16 15:51:20 +0200

    Add subclass Motorcycle

---
+++ ConsoleApp/Vehicles/Motorcycle.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp.Vehicles
+{
+    internal class Motorcycle : Vehicle
+    {
+        public int CylinderVolume { get; }
+        public Motorcycle(string regNumber, string color, int wheels, int cylinderVolume)
+            : base(regNumber, color, wheels)
+        {
+            CylinderVolume = cylinderVolume;
+        }
+    }
+}
