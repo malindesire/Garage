947b52d024af90d9000de7de3e08b835ed475be0
malindesire <malindesirejohansson@gmail.com>
2025-06-16 15:55:48 +0200

    Add subclass Boat

---
+++ ConsoleApp/Vehicles/Boat.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp.Vehicles
+{
+    internal class Boat : Vehicle
+    {
+        public int Length { get; }
+        public Boat(string regNumber, string color, int wheels, int length)
+            : base(regNumber, color, wheels)
+        {
+            Length = length;
+        }
+    }
+}
