7cc8f465c1e0fbdf5956bb36308a573f5da9a0f2
malindesire <malindesirejohansson@gmail.com>
2025-06-16 15:54:10 +0200

    Add subclass Bus

---
+++ ConsoleApp/Vehicles/Bus.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp.Vehicles
+{
+    internal class Bus: Vehicle
+    {
+        public int SeatQty { get; }
+        public Bus(string regNumber, string color, int wheels, int seatQty)
+            : base(regNumber, color, wheels)
+        {
+            SeatQty = seatQty;
+        }
+
+    }
+}
