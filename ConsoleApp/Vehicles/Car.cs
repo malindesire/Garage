a0bf86b905263a0235b81c2d7acf85b61ed20cf0
malindesire <malindesirejohansson@gmail.com>
2025-06-16 15:52:36 +0200

    Add subclass Car

---
+++ ConsoleApp/Vehicles/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp.Vehicles
+{
+    internal class Car : Vehicle
+    {
+        public int Doors { get; }
+        public Car(string regNumber, string color, int wheels, int doors)
+            : base(regNumber, color, wheels)
+        {
+            Doors = doors;
+        }
+    }
+}
