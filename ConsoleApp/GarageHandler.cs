f0e54079862b6e41dcbf32455b15e02f0297eec4
malindesire <malindesirejohansson@gmail.com>
2025-06-17 21:15:32 +0200

    Implement ShowVehicleTypesAndCounts method

--- ConsoleApp/GarageHandler.cs
+++ ConsoleApp/GarageHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp
+﻿using ConsoleApp.Vehicles;
+
+namespace ConsoleApp
 {
     internal class GarageHandler
     {
@@ -17,15 +19,33 @@
                 {
                     _ui.ShowMessage($"Spot {spot.Number}: {spot.ParkedVehicle.GetType().Name} - {spot.ParkedVehicle.RegNumber}");
                 }
-                else
-                {
-                    _ui.ShowMessage($"Spot {spot.Number}: Empty");
-                }
             }
         }
         public void ShowVehicleTypesAndCounts()
         {
             // Implementation for showing vehicle types and counts
+
+            var vehicleCounts = new Dictionary<string, int>(); 
+            foreach (var spot in _garage.Spots)
+            {
+                if (spot.ParkedVehicle != null)
+                {
+                    var vehicleType = spot.ParkedVehicle.GetType().Name;
+                    if (vehicleCounts.ContainsKey(vehicleType))
+                    {
+                        vehicleCounts[vehicleType]++;
+                    }
+                    else
+                    {
+                        vehicleCounts[vehicleType] = 1;
+                    }
+                }
+            }
+
+            foreach (var kvp in vehicleCounts)
+            {
+                _ui.ShowMessage($"{kvp.Key}: {kvp.Value} parked");
+            }
         }
         public void ParkVehicle()
         {
@@ -38,6 +58,24 @@
         public void PopulateGarageWithVehicles(int count)
         {
             // Implementation for populating the garage with vehicles
+            var airplane = new Airplane("ABC123", "white", 60);
+            var boat = new Boat("DEF123", "black", 55);
+            var bus = new Bus("GHI123", "green", 35);
+            var car = new Car("JKL123", "red", 2);
+            var motorcycle = new Motorcycle("MNO123", "yellow", 125);
+
+            _garage.Spots[0].Park(airplane);
+            _garage.Spots[7].Park(airplane);
+            _garage.Spots[8].Park(airplane);
+            _garage.Spots[1].Park(boat);
+            _garage.Spots[2].Park(bus);
+            _garage.Spots[3].Park(car);
+            _garage.Spots[9].Park(car);
+            _garage.Spots[4].Park(motorcycle);
+            _garage.Spots[5].Park(motorcycle);
+            _garage.Spots[6].Park(motorcycle);
+
+            _ui.ShowMessage($"Garage populated with {count} vehicles.");
         }
         public void SearchVehicleByRegistrationNumber()
         {
