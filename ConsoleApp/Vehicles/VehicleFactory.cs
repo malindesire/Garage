16f8532ef87909e7adbef93f58935768a0965d44
malindesire <malindesirejohansson@gmail.com>
2025-06-18 17:53:05 +0200

    Add VehicleFactory with GenerateRandom method

---
+++ ConsoleApp/Vehicles/VehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp.Vehicles
+{
+    internal static class VehicleFactory
+    {
+        private static readonly Random _random = new Random();
+
+        public static Vehicle GenerateRandomVehicle()
+        { 
+            string regNumber = Guid.NewGuid().ToString("N").Substring(0, 6).ToUpper();
+            VehicleColor color = (VehicleColor)_random.Next(0, Enum.GetValues(typeof(VehicleColor)).Length);
+            VehicleType type = (VehicleType)_random.Next(0, Enum.GetValues(typeof(VehicleType)).Length);
+
+            return type switch
+            {
+                VehicleType.Airplane => new Airplane(regNumber, color, _random.Next(20, 80)),
+                VehicleType.Boat => new Boat(regNumber, color, _random.Next(2, 60)),
+                VehicleType.Bus => new Bus(regNumber, color, _random.Next(20, 100)),
+                VehicleType.Car => new Car(regNumber, color, _random.Next(2, 6)),
+                VehicleType.Motorcycle => new Motorcycle(regNumber, color, _random.Next(45, 210)),
+                _ => new Car(regNumber, color, _random.Next(2, 6))
+            };
+        }
+    }
+}
