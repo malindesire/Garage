5fac3c522eeb3ba69bae0ae92a5dff2b1874a767
malindesire <malindesirejohansson@gmail.com>
2025-06-18 20:45:31 +0200

    Add HashSet to keep track of used RegNumber

--- ConsoleApp/Vehicles/Vehicle.cs
+++ ConsoleApp/Vehicles/Vehicle.cs
@@ -2,18 +2,26 @@
 {
     internal abstract class Vehicle
     {
+        private static HashSet<string> _usedRegNumbers = new HashSet<string>();
         public VehicleType Type { get; }
-        //TODO: Make sure regNumber is unique across all vehicles.
         public string RegNumber { get; }
         public VehicleColor Color { get; }
         public int Wheels { get; }
 
         public Vehicle(VehicleType type, string regNumber, VehicleColor color, int wheels)
         {
+            //TODO: Handle exceptions for invalid inputs
+            if (_usedRegNumbers.Contains(regNumber))
+            {
+                throw new ArgumentException($"Registration number '{regNumber}' is already in use.");
+            }
+
             Type = type;
             RegNumber = regNumber.ToUpper();
             Color = color;
             Wheels = wheels;
+            _usedRegNumbers.Add(regNumber.ToUpper());
+
         }
     }
 }
